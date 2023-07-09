12243c56be642e9e096c61480eccfbc1eec41b65
Queenbot524 <135263523+Queenbot524@users.noreply.github.com>
2023-07-09 16:36:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "27353035"))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "5350929381"))
+OWNER_ID = int(getenv("OWNER_ID", "1476655690"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Nexa_Verse")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "DadEyeBotz")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Dev_Arora_0981")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "EBD_QN_CHAT")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "EBD_QN")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "MAFIA_RJ")
 
 # Random Start Images
 IMG = [
